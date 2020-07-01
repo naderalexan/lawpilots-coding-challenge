1c43e56cda1f4df43c2f67509b5377509edccbe5
Nader Alexan <alexan.nader@gmail.com>
2020-07-01 13:41:09 +0200

    chore: test async task execution completion

--- lawpilots/tests.py
+++ lawpilots/tests.py
@@ -1,12 +1,21 @@
 import unittest
+import time
 
 from .tasks import say_hello
 
 
 class TestTasks(unittest.TestCase):
+    def setUp(self):
+        self.name = "Nader"
+
     def test_say_hello(self):
-        name = "Nader"
-        self.assertEqual(say_hello(name), f"Hello {name}")
+        self.assertEqual(say_hello(self.name), f"Hello {self.name}")
+
+    def test_task_status(self):
+        task = say_hello.delay(self.name)
+        while not task.ready():
+            time.sleep(0.1)
+        self.assertEqual(task.status, "SUCCESS")
 
 
 if __name__ == "__main__":
